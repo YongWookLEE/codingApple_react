63447fda8c7f4d5b6559b1adcd1c4780465fb6ac
lyw1008 <lyw1008@danusys.com>
2023-05-18 18:03:13 +0900

    리덕스 store 적용

--- shop/src/routes/Cart.js
+++ shop/src/routes/Cart.js
@@ -1,24 +1,41 @@
 import { Table } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { changeNm, changeAge } from "../data/store";
 
 const Cart = () => {
+  let data = useSelector((state) => state.cart);
+  let user = useSelector((state) => state.user);
+  let dispatch = useDispatch();
+  console.log(user.age);
   return (
     <div>
+      {user.name}의 장바구니
       <Table>
         <thead>
           <tr>
-            <th>#</th>
+            <th>상품번호</th>
             <th>상품명</th>
             <th>수량</th>
             <th>변경하기</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>안녕</td>
-            <td>안녕</td>
-            <td>안녕</td>
-          </tr>
+          {data.map((d) => (
+            <tr>
+              <td>{d.id}</td>
+              <td>{d.name}</td>
+              <td>{d.count}</td>
+              <td>
+                <button
+                  onClick={() => {
+                    dispatch(changeNm());
+                  }}
+                >
+                  +
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
