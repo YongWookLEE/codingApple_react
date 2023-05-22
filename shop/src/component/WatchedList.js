49a0bb01c1c646126583749a2bed2cc005cc2665
YongWookLEE <lsd1008@naver.com>
2023-05-22 23:06:29 +0900

    최근 본 리스트 CSS 수정

--- shop/src/component/WatchedList.js
+++ shop/src/component/WatchedList.js
@@ -5,15 +5,17 @@ function WatchedList() {
     const watchedList = JSON.parse(localStorage.getItem('watched'));
     let navigate = useNavigate();
   return (
-    <ListGroup horizontal>
-        {
-            watchedList.map((i)=> {
-            return <ListGroup.Item onClick={() => {
-                navigate(`/detail/${i.id}`,{state: {...i}} )
-            }
-            }>{i.title}</ListGroup.Item>})
-        }
-    </ListGroup>
+    <div className='watched'>  
+      <ListGroup key='xl' horizontal='xl' className="my-2">
+          {
+              watchedList.map((i)=> {
+              return <ListGroup.Item onClick={() => {
+                  navigate(`/detail/${i.id}`,{state: {...i}} )
+              }
+              }>{i.title}</ListGroup.Item>})
+          }
+      </ListGroup>
+    </div>
   );
 }
 
